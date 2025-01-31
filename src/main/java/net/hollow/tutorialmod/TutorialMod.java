4b14c638acdf9fab74c9389e34b2fdbe9646449c
HollowEnd <HollowsEndDev@gmail.com>
2025-01-30 22:51:43 -0500

    Update Added Items

--- src/main/java/net/hollow/tutorialmod/TutorialMod.java
+++ src/main/java/net/hollow/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.hollow.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.hollow.tutorialmod.Item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -32,7 +34,7 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -48,7 +50,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+     if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+     {
+         event.accept(ModItems.ALEXANDRITE);
+         event.accept(ModItems.RAW_ALEXANDRITE);
+     }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
